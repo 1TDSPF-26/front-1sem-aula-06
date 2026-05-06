ba03d371ed600216dcaaa010df672210aa9cde7c
luisfelipe-fernandesesteves <luisfelipefernandes.corp@gmail.com>
2026-05-06 11:25:26 -0300

    feat: comentarios sobre o dom

--- js/script.js
+++ js/script.js
@@ -1,1 +1,6 @@
 //forma correta de utilizar java script
+//java scrip é utilizado para manipular o html e o css
+//para isso utilizamos o DOM (Document Object Model)
+//DOM é uma representação do documento html em forma de árvore
+//cada elemento do html é representado por um nó da árvore
+//para acessar os elementos do html utilizamos os métodos do DOM
