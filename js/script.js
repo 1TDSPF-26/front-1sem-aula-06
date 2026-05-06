f70bee007c2ca98041f8439d64d511942a21072d
MatheusRodriguesSerrao <rmatheusrodrigues193@gmail.com>
2026-05-06 11:53:54 -0300

    feat: Implementar funcionalidade de login com botão e evento de clique

---
+++ js/script.js
@@ -0,0 +1,10 @@
+
+const botaoEntrar = document.getElementById("btnEntrar");
+
+//Atrelando um evento de ação ao botão capturado
+//Utilizamos a função addEventListener como perceptora(escutador de eventos) ao botão, o evento vai ser o click.
+//Este evento quando usado no HTML utilizamos o prefixo ON, mas aqui no JS, não utilizamos, então ao invés de usar onclick, usamos apenas click.
+//A função addEventListener(param1,param2), recebe dois parâmetros, o primeiro é o evento que se espera, o segundo é uma função anônima ou de callBack.
+botaoEntrar.addEventListener("click", function(){
+    console.log("Botão ativo!");
+});
