ad58e75d93e2daac30010edff7822537b12c7a4a
alecarlosjesus <profalexandre.jesus@email.com.br>
2026-05-12 00:23:11 -0300

    assets
    Signed-off-by: alecarlosjesus <profalexandre.jesus@email.com.br>

--- js/script.js
+++ js/script.js
@@ -8,10 +8,32 @@ const botaoEntrar = document.getElementById("btnEntrar");
 botaoEntrar.addEventListener("click", function(){
     
     //Recupere aqui o campo email:
-
+    const email = document.getElementById("idEmail");
+    
     //Recupere aqui o campo senha:
+    const senha = document.getElementById("idSenha");
 
     //Imprimir no console log ambos os dados dos campos:
+    console.log(email.value);
+    console.log(senha.value);
+
+    //Declarar um objeto e inserir os dados dos campos:
+    const usuario = {
+        email: email.value,
+        senha: senha.value
+    }
+
+
+    //Fazer uma validação de email e senha, se estiverem preenchidos, então mostrar o alerta "Bem vindo ao sistema.
+    if (email.value && senha.value) {
+        const modal = document.getElementById("modal-login");
+        const closeModal = document.getElementsByClassName("modal__close")[0];
+        modal.showModal();
+        closeModal.addEventListener("click", function(){
+            modal.close();
+        })
+    
+    }
 
 
 });
